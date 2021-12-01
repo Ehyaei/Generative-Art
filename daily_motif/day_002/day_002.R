b9c584ec24de80b69f13cbb6e997b10b78841d79
Ahmad <ahmad.ehyaei@gmail.com>
2021-12-01 23:04:04 +0100

    day002

---
+++ daily_motif/day_002/day_002.R
@@ -0,0 +1,5 @@
+library(ggplot2)
+library(Kaashi)
+square = regularPolygon(8)
+tile <- motif(square,n = 8, theta = 45, delta = 0.5, polyLine = F, dist = 0.04)
+daily_minimal_plot(tile,"daily_motif/day_001/day_001.png",type = "png",caption = "Daily Motif\n\nNO. 1")
