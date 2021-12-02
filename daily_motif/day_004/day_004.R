d95bd2c813acd1e52646a8946f494fae3050cfe3
Ahmad <ahmad.ehyaei@gmail.com>
2021-12-02 08:42:50 +0100

    day_004

--- daily_motif/day_004/day_004.R
+++ daily_motif/day_004/day_004.R
@@ -1,10 +1,10 @@
 library(ggplot2)
 library(Kaashi)
-square = regularPolygon(7)
-tile <- motif(square,n = 7, theta = 30, delta = 1, polyLine = F, dist = 0.05,circle = T,radius = 2)
+square = regularPolygon(5)
+tile <- motif(square,n = 4,  delta = 0.5, polyLine = F,midpoint = c(1,1), dist = 0.05,circle = T,radius = 1.2)
 
-daily_minimal_plot(tile,"daily_motif/day_004/day_004.png",type = "png",caption = "Daily Motif\n\nNO. 3")
-daily_minimal_plot(tile,"daily_motif/day_004/day_004.svg",type = "svg",caption = "Daily Motif\n\nNO. 3")
+daily_minimal_plot(tile,"daily_motif/day_004/day_004.png",type = "png",caption = "Daily Motif\n\nNO. 4")
+daily_minimal_plot(tile,"daily_motif/day_004/day_004.svg",type = "svg",caption = "Daily Motif\n\nNO. 4")
 
 copy_path <- "../../Blog/content/gallery/2021-11-30-daily-motif/images/"
 folder_path <-  "daily_motif/day_004/"
