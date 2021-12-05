15ead0594a6036be679b21e1e84fb952d951199c
Ahmad <ahmad.ehyaei@gmail.com>
2021-12-05 21:58:44 +0100

    design day 008

--- daily_motif/day_008/day_008.R
+++ daily_motif/day_008/day_008.R
@@ -1,6 +1,6 @@
 source("manuscript/utils.R")
 
 square <- regularPolygon(4)
-tile <- motif(square, n = 9, theta = 40, delta = 0.5, polyLine = F, dist = 0.07,midpoint = c(1,-1))
-daily_submit(tiles, "9")
+tile <- motif(square, n = 9, theta = 40, delta = 0.5, polyLine = F, dist = 0.06,midpoint = c(1,-1))
+daily_submit(tile, "8")
 
