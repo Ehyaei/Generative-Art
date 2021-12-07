6a74f5cbc7ecc7b6d02205d6cc6c9a4eef3a95cc
Ahmad <ahmad.ehyaei@gmail.com>
2021-12-07 20:46:07 +0100

    design day 009

--- daily_motif/day_009/day_009.R
+++ daily_motif/day_009/day_009.R
@@ -2,8 +2,8 @@ source("manuscript/utils.R")
 
 center <- regularPolygon(3,sf = T) %>% st_centroid() %>% .$geometry %>% unlist()
 triangle <- regularPolygon(3)
-tile1 <- motif(box = triangle, theta = 45, delta = 0.5, n = 6, dist = 0.02, midpoint = center)
-tile2 <- motif_rotation(tile1,theta = 180) %>% motif_transfer(c(0,0.001))
+tile1 <- motif(box = triangle, theta = 45, delta = 0.5, n = 6, dist = 0.03, midpoint = center)
+tile2 <- motif_rotation(tile1,theta = 180) %>% motif_transfer(c(0,0.00))
 tile <- rbind(tile1, tile2) %>% motif_rotation(90)
 tile %>% filter(area < max(tile1$area)) -> tile
 tilePlotter(tile)
