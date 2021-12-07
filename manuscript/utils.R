db6747ce783894ea39974be7c34c169f82cf17ef
Ahmad <ahmad.ehyaei@gmail.com>
2021-12-07 20:53:22 +0100

    design day 009

--- manuscript/utils.R
+++ manuscript/utils.R
@@ -15,7 +15,7 @@ library(sf)
 ############################################################
 
 plot_transfer <- function(folder_path, pettern_name, copy_path){
-  file.copy(paste0(folder_path,"/",pettern_name,".png"), copy_path)
+  file.copy(paste0(folder_path,"/",pettern_name,".png"), copy_path, overwrite = T)
   md_string <- paste0("![](",pettern_name,".svg",")")
   file.create(paste0(folder_path,"/README.md"))
   writeLines(md_string, paste0(folder_path,"/README.md"))
