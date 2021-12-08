17f373b5bfdd0082ec8d16fc95206b492330ba0a
Ahmad <ahmad.ehyaei@gmail.com>
2021-12-08 20:05:47 +0100

    design day 010

--- daily_motif/day_010/day_010.R
+++ daily_motif/day_010/day_010.R
@@ -1,7 +1,7 @@
 source("manuscript/utils.R")
 
 circle <- st_buffer(st_point(c(0,0)),1) %>% unclass() %>% .[[1]]
-tile1 <- motif(box = circle, theta = 45, delta = 0.5, n = 6, dist = 0.01)
+tile <- motif(box = circle, theta = 30, delta = 0, n = 11, dist = 0.03, midpoint = c(0.5, 0.5))
 tilePlotter(tile)
 daily_submit(tile, "10")
 
