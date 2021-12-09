e82a16a64bc884090c2fcf771a9f80c8831ec05c
Ahmad <ahmad.ehyaei@gmail.com>
2021-12-09 23:32:49 +0100

    design day 011

---
+++ daily_motif/day_011/day_011.R
@@ -0,0 +1,10 @@
+source("manuscript/utils.R")
+circle <- st_buffer(st_point(c(0,1)),.6)
+square = regularPolygon(4)
+tile <- motif(box = square, theta = 60, delta = 0, n = 4, dist = 0.03,circle = T,radius = 1)
+tile = st_difference(tile,circle)
+tiles = tiling(tile, n = 2)
+tilePlotter(tiles)
+plot(tiles)
+daily_submit(tile, "11")
+
