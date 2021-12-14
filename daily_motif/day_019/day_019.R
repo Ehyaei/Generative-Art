0096a767d7c2e2beb8c841705a8b07a254a56aa6
Ahmad <ahmad.ehyaei@gmail.com>
2021-12-14 19:47:45 +0100

    design day 019

--- daily_motif/day_019/day_019.R
+++ daily_motif/day_019/day_019.R
@@ -3,6 +3,6 @@ n = 6
 polygon = regularPolygon(n)
 center = st_centroid(regularPolygon(n,sf = T)) %>% .$geometry %>% unlist()
 tile <- motif(box = polygon, n = 2*n, theta = c(30, 60), delta = 0.5, dist =  0.00000001)
-tile <- tile %>% filter(area < 0.07)
+tile <- tile %>% filter(area > 0.07)
 tilePlotter(tile)
 daily_submit(tile, "19")
