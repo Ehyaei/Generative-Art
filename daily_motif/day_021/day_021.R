c1a859b4a0ef4bbaed4c936e0c27847b9ad402dd
Ahmad <ahmad.ehyaei@gmail.com>
2021-12-16 23:58:02 +0100

    design day 021

---
+++ daily_motif/day_021/day_021.R
@@ -0,0 +1,12 @@
+source("manuscript/utils.R")
+
+d = 0.0001
+n = 6
+polygon = regularPolygon(n)
+center = st_centroid(regularPolygon(n,sf = T)) %>% .$geometry %>% unlist()
+tile <- motif(box = polygon, n = 6, theta = c(30,60), delta = 0,circle = T, dist =  d,radius = 2.2) %>% 
+  filter(area>min(area)) %>%
+  filter(area>min(area)) %>%
+  filter(area>min(area))
+tiles = tiling(tile, box = polygon, n = 2, type = "hexagonal")
+daily_submit(tile, "21")
